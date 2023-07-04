9ce7156067fd5fb9677264b994cec518059d48f0
Gustavo Saraiva <124311748+saraivagustavo@users.noreply.github.com>
2023-07-04 00:29:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+
+
+function escrevendoLetra(){
+        function ativaLetra(elemento){
+            const arrayTexto = elemento.innerHTML.split('');
+            elemento.innerHTML = '';
+            arrayTexto.forEach((letra, i) =>{
+                setTimeout(()=>{
+                    elemento.innerHTML += letra;
+                }, 75 * i);
+            });
+        }
+        
+        const titulo = document.querySelector('.digitando')
+        ativaLetra(titulo);
+}
+
+    escrevendoLetra()
+
+function menuMobile(){
+        const ativaMenu = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('header .navegacao-primaria')
+    
+    ativaMenu.addEventListener('click', ()=>{
+        ativaMenu.classList.toggle('fa-x');
+        navMenu.classList.toggle('ativado');
+    });
+}
+
+function sobreMim(){
+ 
+const divExperiencia = document.querySelectorAll('.experience_content div');
+const liExperiencia = document.querySelectorAll('.experience_content ul li');
+const divEducation = document.querySelectorAll('.education_content div');
+const liEducation = document.querySelectorAll('.education_content ul li');
+
+divExperiencia[0].classList.add('ativo')
+divEducation[0].classList.add('ativo')
+liExperiencia[0].classList.add('ativo')
+liEducation[0].classList.add('ativo')
+
+function slideShow(index){
+    divExperiencia.forEach((div)=>{
+        div.classList.remove('ativo');
+    });
+    liExperiencia.forEach((botao)=>{
+        botao.classList.remove('ativo');
+    });
+    divExperiencia[index].classList.add('ativo')
+    liExperiencia[index].classList.add('ativo')
+}
+
+function slideShow2(index){
+    divEducation.forEach((div)=>{
+        div.classList.remove('ativo');
+    });
+    liEducation.forEach((botao)=>{
+        botao.classList.remove('ativo');
+    });
+    divEducation[index].classList.add('ativo')
+    liEducation[index].classList.add('ativo')
+ }
+
+    liExperiencia.forEach((event,index)=>{
+    event.addEventListener('click',()=>{
+        slideShow(index)
+    });
+ });
+    liEducation.forEach((event,index)=>{
+    event.addEventListener('click',()=>{
+        slideShow2(index)
+    });
+ });
+
+}
+
+sobreMim();
+
+
+const listaAll = document.querySelectorAll('.project_armazenamento ul li');
+const buttonGeral = document.querySelectorAll('.project_models ul li');
+const buttonAll = document.querySelectorAll('.project_models .all');
+
+function removeClick(index){
+    buttonGeral.forEach((item)=>{
+        item.classList.remove('ativo');
+    });
+    buttonGeral[index].classList('ativo');
+}
+
+buttonAll.forEach((item,index)=>{
+    item.addEventListener('click', ()=>{
+        removeClick(index);
+    })
+})
+
+function showLista(lista, button = 'all'){
+    lista.forEach((item)=>{
+        item.classList.remove('ativo');
+    })
+    if(button == 'repository'){
+        lista[0].classList.add('ativo');
+        lista[1].classList.add('ativo');
+    }
+    if(button == 'website'){
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+    }
+}
+
+buttonAll.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let currentButton = e.target;
+        if(currentButton.classList.contains('all')){
+            showLista(listaAll);        
+        }
+
+        if(currentButton.classList.contains('repository')){
+            showLista(listaAll, "repository");        
+        }
+
+        if(currentButton.classList.contains('website')){
+            showLista(listaAll, "website");        
+        }
+
+        if(currentButton.classList.contains('all')){
+            showLista(listaAll, "all");        
+        }
+    })
+})
